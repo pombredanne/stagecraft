8b80ad88ab2b145c7cd1bf7b98fca433f3c0e4f6
Jacob Ashdown <jcbashdown@gmail.com>
2014-07-11 16:00:02 +0000

    more moving things around,
    comment out and just get skeleton of migration and then start writing test

--- stagecraft/apps/datasets/migrations/0013_carers_claims_to_transactions_by_channel.py
+++ stagecraft/apps/datasets/migrations/0013_carers_claims_to_transactions_by_channel.py
@@ -1,145 +1,32 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
 from south.db import db
-from south.v2 import DataMigration
+from south.v2 import SchemaMigration
 from django.db import models
-from django.db.models import Q
-from django.conf import settings
-
-import pprint as pp
-import random
-import json
-from performanceplatform.client import DataSet as client
-BEARER_TOKEN_LENGTH = 64
-
-base_url = 'https://www.preview.performance.service.gov.uk'
-
-input_sets = [
-    'carers_allowance_monthly_claims',
-    'carers_allowance_weekly_claims'
-]
-
-output_set = 'carers_allowance_transactions_by_channel'
-
-key_mapping = {
-    "key": "channel",
-    "value": "count"
-}
-
-value_mapping = {
-    "ca_clerical_received": "paper",
-    "ca_e_claims_received": "digital"
-}
-
-
-def _get_output_data_set(token=None):
-    data_set = client.from_name(base_url, output_set)
-    if token is not None:
-        data_set.set_token(token)
-    else:
-        data_set.set_token(settings.STAGECRAFT_DATA_SET_QUERY_TOKEN)
-    return data_set
-
-
-def _generate_bearer_token():
-    """
-    >>> len(_generate_bearer_token()) == BEARER_TOKEN_LENGTH
-    True
-    >>> import re
-    >>> regex = re.compile("^[(a|b|c|d|e|f|g|h|j|k|m|n|p|q|r|s|t|u|v|w|x|y|z|2|3|4|5|6|7|8|9)]{10,}$")
-    >>> type(regex.match(_generate_bearer_token()))
-    <type '_sre.SRE_Match'>
-    """
-    chars = "abcdefghjkmnpqrstuvwxyz23456789"
-    token = "".join(random.choice(chars) for _ in range(BEARER_TOKEN_LENGTH))
-    return token
-
-
-def get_data_from_claims_sets():
-    input_data = []
-    for set_name in input_sets:
-        data_set = client.from_name(base_url, set_name)
-        for item in data_set.get().json()['data']:
-            input_data.append(item)
-    return input_data
-
-
-def apply_new_key_mappings(document):
-    for key, val in document.items():
-        if key in key_mapping:
-            document.pop(key)
-            document[key_mapping[key]] = val
-        else:
-            document[key] = val
-    return document
-
-
-def apply_new_values(document):
-    for key, val in document.items():
-        if val in value_mapping:
-            document['comment'] = str(document['comment']) + " / " + val
-            document[key] = value_mapping[val]
-    return document
-
-
-def build_documents(documents):
-    docs = []
-    for document in documents:
-        doc = apply_new_values(apply_new_key_mappings(document))
-        docs.append(doc)
-    return docs
-
-
-def post_docs_to_production(documents):
-    data_set = _get_output_data_set()
-    data_set.post(documents)
-
-
-def clear_docs_from_output_set():
-    data_set = _get_output_data_set()
-    data_set.empty_data_set()
-
-
-def get_or_create_carers_transactions_by_channel(orm):
-    try:
-        return orm['datasets.DataSet'].objects.get(
-            name=output_set
-        )
-    except orm['datasets.DataSet'].DoesNotExist:
-        by_transaction = orm['datasets.DataSet'].objects.create(
-            name=output_set,
-            data_group='carers-allowance',
-            data_type='transactions-by-channel',
-            bearer_token=_generate_bearer_token(),
-            upload_format='excel',
-            upload_filters='backdrop.core.upload.filters.first_sheet_filter',
-            max_age_expected=2678400,
-            published=True
-        )
-        return by_transaction
-
-transaction_by_channel_data_set_mappings = [
-        {
-            'old_name': "some_name",
-            'new_data_set': {
-                    'some_attribute': "a",
-                    'name': "b"
-                },
-            'data_mapping': {
-                    'key_mapping': {
-                            "key": "channel",
-                            "value": "count"
-                        },
-                    'value_mapping': {
-                            "ca_clerical_received": "paper",
-                            "ca_e_claims_received": "digital"
-                        }
-                }
-        }
-    ]
 
 class Migration(DataMigration):
+
+    transaction_by_channel_data_set_mappings = [
+            {
+                'old_name': "some_name",
+                'new_data_set': {
+                        'some_attribute': "a",
+                        'name': "b"
+                    },
+                'data_mapping': {
+                        'key_mapping': {
+                                "key": "channel",
+                                "value": "count"
+                            },
+                        'value_mapping': {
+                                "ca_clerical_received": "paper",
+                                "ca_e_claims_received": "digital"
+                            }
+                    }
+            }
+        ]
+
 
     def forwards(self, orm):
         #non destructive - the old ones hang around in case there is a problem
