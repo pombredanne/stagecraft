8b763812192d047c6045cb1c3cc0dd3e415f4bee
Jacob Ashdown <jcbashdown@gmail.com>
2014-10-03 17:21:00 +0100

    wip remove old schemas - this is js
    add way to run and proper loading too

--- tools/import_schemas/import.py
+++ tools/import_schemas/import.py
@@ -1,19 +1,32 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
 from stagecraft.apps.dashboards.models.module import ModuleType
 from dictdiffer import diff
 import os
 import json
 
+# run it like this:
+# python -m tools.import_schemas.import
+
 
 def get_schema_for_module_type(name):
     path = os.path.join(
         os.path.dirname(__file__),
-        'schema/dashboard.json')
-    with open(path, "r") as file:
-        schema = file.read()
+        'schema/modules_json/{}_schema.json'.format(name))
+    try:
+        with open(path, "r") as file:
+            schema = file.read()
+    except IOError as e:
+        path = os.path.join(
+            os.path.dirname(__file__),
+            'schema/module.json'.format(name))
+        with open(path, "r") as file:
+            schema = file.read()
     schema_dict = json.loads(schema)
     return schema_dict
 
 
 if __name__ == '__main__':
     for module_type in ModuleType.objects.all():
-        print get_schema_for_module_type(module_type.name)
+        get_schema_for_module_type(module_type.name)
